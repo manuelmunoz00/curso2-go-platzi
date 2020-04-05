7768d3e21d0ad73e5fdd3fcbb3a3709a0e1992e3
Manuel Muñoz <mmunoz@digital.gob.cl>
2020-04-05 16:35:04 -0400

    se agrega listado

--- tareas.go
+++ tareas.go
@@ -8,6 +8,14 @@ type task struct {
 	prioridad           int
 }
 
+type taskList struct {
+	tasks []*task
+}
+
+func (tl *taskList) agregarALista(tarea *task) {
+	tl.tasks = append(tl.tasks, tarea)
+}
+
 func (t *task) marcarCompleta() {
 	t.completado = true
 }
@@ -21,22 +29,37 @@ func (t *task) actualizarNombre(nombre string) {
 }
 
 func main() {
-	t := task{
+	tarea1 := &task{
 		nombre:      "Tomar desayuno",
 		descripcion: "Tomar leche y comer pan",
 		completado:  false,
 	}
-	fmt.Printf("%+v\n", t)
-	t.marcarCompleta()
-	t.actualizarNombre("Almorzar")
-	t.actualizarDescripcion("Plato de almuerzo con ensalada")
-	fmt.Printf("%+v\n", t)
-	var tarea2 task
-	tarea2 = task{
-		descripcion: "desc",
-		nombre:      "nomb",
+	// fmt.Printf("%+v\n", t)
+	// t.marcarCompleta()
+	// t.actualizarNombre("Almorzar")
+	// t.actualizarDescripcion("Plato de almuerzo con ensalada")
+	// fmt.Printf("%+v\n", t)
+	tarea2 := &task{
+		descripcion: "Cazuela de pollo y ensalada",
+		nombre:      "Almorzar",
 		prioridad:   1,
 		completado:  true,
 	}
-	fmt.Println(tarea2)
+	// fmt.Println(tarea2)
+
+	tarea3 := &task{
+		descripcion: "Fideos con salsa de tomate",
+		nombre:      "Cenar",
+		prioridad:   1,
+		completado:  true,
+	}
+
+	lista := &taskList{
+		tasks: []*task{
+			tarea1, tarea2,
+		},
+	}
+	fmt.Println(lista.tasks[0])
+	lista.agregarALista(tarea3)
+	fmt.Println(len(lista.tasks))
 }
