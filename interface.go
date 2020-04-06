6227d636846c5d8080f897e16f3867432b44c0b6
Manuel Muñoz <manuel.munoz00@gmail.com>
2020-04-05 22:29:36 -0400

    ejemplo de interface con structs que adoptan comportamiento

---
+++ interface.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type perro struct {
+}
+
+type pescado struct {
+}
+
+type ave struct {
+}
+
+//Los structs anteriores deben implementar el metodo mover para cada uno que es requisito para la interfaz de tipo animal
+func (perro) mover() string {
+	return "Soy un perro y estoy caminando"
+}
+
+func (pescado) mover() string {
+	return "Sour un pescado y estoy nadando"
+}
+
+func (ave) mover() string {
+	return "Soy un ave y estoy volando"
+}
+
+//Interfaz de tipo animal
+type animal interface {
+	mover() string
+}
+
+//Función que recibe un animal
+func moverAnimal(a animal) {
+	fmt.Println(a.mover())
+}
+
+func main() {
+	pr := perro{}
+	moverAnimal(pr)
+	pc := pescado{}
+	moverAnimal(pc)
+	av := ave{}
+	moverAnimal(av)
+}
