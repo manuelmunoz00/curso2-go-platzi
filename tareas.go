69704c919050ddc00a07bc8989a560b596ad49ad
Manuel Muñoz <manuel.munoz00@gmail.com>
2020-04-05 22:26:04 -0400

    se implementa ejemplo de interfaz animal y structs que adoptan metodo

--- tareas.go
+++ tareas.go
@@ -37,57 +37,57 @@ func (tl *taskList) imprimirListaCompletados() {
 	}
 }
 
-func main() {
-	tarea1 := &task{
-		nombre:      "Tomar desayuno",
-		descripcion: "Tomar leche y comer pan",
-		completado:  false,
-	}
-	// fmt.Printf("%+v\n", t)
-	// t.marcarCompleta()
-	// t.actualizarNombre("Almorzar")
-	// t.actualizarDescripcion("Plato de almuerzo con ensalada")
-	// fmt.Printf("%+v\n", t)
-	tarea2 := &task{
-		descripcion: "Cazuela de pollo y ensalada",
-		nombre:      "Almorzar",
-		prioridad:   1,
-		completado:  false,
-	}
-	// fmt.Println(tarea2)
+// func main() {
+// 	tarea1 := &task{
+// 		nombre:      "Tomar desayuno",
+// 		descripcion: "Tomar leche y comer pan",
+// 		completado:  false,
+// 	}
+// 	// fmt.Printf("%+v\n", t)
+// 	// t.marcarCompleta()
+// 	// t.actualizarNombre("Almorzar")
+// 	// t.actualizarDescripcion("Plato de almuerzo con ensalada")
+// 	// fmt.Printf("%+v\n", t)
+// 	tarea2 := &task{
+// 		descripcion: "Cazuela de pollo y ensalada",
+// 		nombre:      "Almorzar",
+// 		prioridad:   1,
+// 		completado:  false,
+// 	}
+// 	// fmt.Println(tarea2)
 
-	tarea3 := &task{
-		descripcion: "Fideos con salsa de tomate",
-		nombre:      "Cenar",
-		prioridad:   1,
-		completado:  true,
-	}
+// 	tarea3 := &task{
+// 		descripcion: "Fideos con salsa de tomate",
+// 		nombre:      "Cenar",
+// 		prioridad:   1,
+// 		completado:  true,
+// 	}
 
-	lista := &taskList{
-		tasks: []*task{
-			tarea1, tarea2,
-		},
-	}
-	fmt.Println(lista.tasks[0])
-	lista.agregarALista(tarea3)
-	fmt.Println(len(lista.tasks))
+// 	lista := &taskList{
+// 		tasks: []*task{
+// 			tarea1, tarea2,
+// 		},
+// 	}
+// 	fmt.Println(lista.tasks[0])
+// 	lista.agregarALista(tarea3)
+// 	fmt.Println(len(lista.tasks))
 
-	for i := 0; i < len(lista.tasks); i++ {
-		fmt.Println("Index", i, "nombre", lista.tasks[i].nombre)
-	}
+// 	for i := 0; i < len(lista.tasks); i++ {
+// 		fmt.Println("Index", i, "nombre", lista.tasks[i].nombre)
+// 	}
 
-	for index, tarea := range lista.tasks {
-		fmt.Println("Index", index, "nombre", tarea.nombre)
-	}
-	lista.tasks[0].marcarCompleta()
-	fmt.Println("Listado de tareas completadas")
-	lista.imprimirListaCompletados()
+// 	for index, tarea := range lista.tasks {
+// 		fmt.Println("Index", index, "nombre", tarea.nombre)
+// 	}
+// 	lista.tasks[0].marcarCompleta()
+// 	fmt.Println("Listado de tareas completadas")
+// 	lista.imprimirListaCompletados()
 
-	//Se agrega mapa
-	mapa := make(map[string]*taskList)
-	mapa["manuel"] = lista
-	mapa["juan"] = lista
-	fmt.Println("Lista de tareas de Manuel")
-	mapa["manuel"].imprimirListaCompletados()
+// 	//Se agrega mapa
+// 	mapa := make(map[string]*taskList)
+// 	mapa["manuel"] = lista
+// 	mapa["juan"] = lista
+// 	fmt.Println("Lista de tareas de Manuel")
+// 	mapa["manuel"].imprimirListaCompletados()
 
-}
+// }
