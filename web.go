52484224041e206136748cc7763db78e8ce4e048
Manuel Muñoz <mmunoz@digital.gob.cl>
2020-04-06 21:47:56 -0400

    se implementa interfaz para leer contenido de sitio web

--- web.go
+++ web.go
@@ -1,9 +1,23 @@
 package main
 
-// func main() {
-// 	respuesta, error := http.Get("https://google.cl")
-// 	if error != nil {
-// 		fmt.Println("error al obtener el contenido", error)
-// 	}
-// 	fmt.Println(respuesta.Body)
-// }
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type escritorweb struct{}
+
+func (escritorweb) Write(p []byte) (int, error) {
+	fmt.Println(string(p))
+	return len(p), nil
+}
+
+func main() {
+	respuesta, error := http.Get("https://google.cl")
+	if error != nil {
+		fmt.Println("error al obtener el contenido", error)
+	}
+	e := escritorweb{}
+	io.Copy(e, respuesta.Body)
+}
